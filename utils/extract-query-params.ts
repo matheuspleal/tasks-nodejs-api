79bf1b1914ca054b3ededd95aa69ff9b9144fd3b
Matheus Leal <matheusspleal@gmail.com>
2023-02-12 18:53:23 -0300

    feat: handling url and query params

---
+++ utils/extract-query-params.ts
@@ -0,0 +1,10 @@
+export function extractQueryParams(query: string): any {
+  return query
+    .substr(1)
+    .split('&')
+    .reduce((queryParams: any, param: string) => {
+      const [key, value] = param.split('=')
+      queryParams[key] = value
+      return queryParams
+    }, {})
+}
